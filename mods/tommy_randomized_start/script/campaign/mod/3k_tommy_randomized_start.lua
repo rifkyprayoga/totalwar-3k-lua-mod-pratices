08443d95216da42d3c63059325b5bbe105761242
tommyZZM <452490446@qq.com>
2020-03-25 23:33:07 +0800

    update improve random position generating

--- mods/tommy_randomized_start/script/campaign/mod/3k_tommy_randomized_start.lua
+++ mods/tommy_randomized_start/script/campaign/mod/3k_tommy_randomized_start.lua
@@ -13,6 +13,14 @@ function _debug:try(what)
   return result
 end
 
+local function random_split_2(min, range)
+  local rangeToRand = (range - min - 2);
+  math.randomseed(os.time());
+  local randIntl = (math.random() * rangeToRand) - (rangeToRand * 2)
+  local randIntlHalf = (randIntl / 2);
+  return ((randIntlHalf / math.abs(randIntlHalf)) * min) + randIntlHalf
+end
+
 local function tommy_is_faction_local_human(context, faction)
   local model = context:query_model();
   return faction:is_human() and faction:name() == model:local_faction():name();
@@ -73,7 +81,8 @@ local function tommy_get_primary_military_force_position(faction)
 end
 
 local MAPPING_PICKED_POSITIONS_COUNT = {};
-local function _get_random_region_position_prevent_duplicated(region_name_list_patched, initial_positions_of_region_expand)
+local function _get_random_region_position_prevent_duplicated(region_name_list_patched,
+                                                              initial_positions_of_region_expand)
   local attemp_max = 3;
   local attemp = 1;
   local found_position = nil
@@ -87,15 +96,15 @@ local function _get_random_region_position_prevent_duplicated(region_name_list_p
       math.randomseed(os.time());
       local index_random = math.random(#position_list);
       local position = position_list[index_random];
-      if (not is_nil(MAPPING_PICKED_POSITIONS_COUNT[""..region_name_random_picked.."_"..index_random])) then
+      if (not is_nil(MAPPING_PICKED_POSITIONS_COUNT["" .. region_name_random_picked .. "_" .. index_random])) then
         found_position = position;
         found_region_name = region_name_random_picked;
-        MAPPING_PICKED_POSITIONS_COUNT[""..region_name_random_picked.."_"..index_random] = true;
+        MAPPING_PICKED_POSITIONS_COUNT["" .. region_name_random_picked .. "_" .. index_random] = true;
       elseif (attemp >= 3) then
         local x, y = unpack(position);
-        found_position = { x + cm:random_number(20) - 40, y + cm:random_number(20) - 40 };
+        found_position = {x + random_split_2(0.66, 6), y + random_split_2(0.66, 6)};
         found_region_name = region_name_random_picked;
-        MAPPING_PICKED_POSITIONS_COUNT[""..region_name_random_picked.."_"..index_random] = true;
+        MAPPING_PICKED_POSITIONS_COUNT["" .. region_name_random_picked .. "_" .. index_random] = true;
       end
     end
     attemp = attemp + 1;
@@ -123,41 +132,39 @@ local function tommy_randomize_all_characters_for_faction(context, faction, init
   local is_faction_has_no_region = region_list:num_items() == 0;
   local is_faction_has_1_region = region_list:num_items() == 1;
 
+  function get_region_name_list_from_adjacent()
+    local result = {};
+    local region_list_normal = region_list_adjacent_normal;
+    for r = 1, #region_list_normal do result[r] = region_list_normal[r]:name(); end
+    out("tommy_randomize_all_characters_for_faction() | " ..faction:name().. " | from adjacent region," .. #result);
+    return result;
+  end
+
+  function get_region_name_list_from_all()
+    local result = {};
+    local region_list_normal = region_list_world_normal;
+    for r = 1, #region_list_normal do result[r] = region_list_normal[r]:name(); end
+    out("tommy_randomize_all_characters_for_faction() | " ..faction:name().. " | from all region," .. #result);
+    return result;
+  end
+
   if (faction:name() == "3k_main_faction_han_empire") then
     -- noop
     region_name_list = {}
   elseif (faction:name() == "3k_main_faction_liu_bei") then
     -- 190刘备, 选择初始区域的临近区域
-    region_list_normal = region_list_adjacent_normal;
-    for r = 1, #region_list_normal do region_name_list[r] = region_list_normal[r]:name(); end
-    out(
-      "tommy_randomize_all_characters_for_faction() | from adjacent region_list:num_items():" .. region_list:num_items() .. " -> " ..
-        #region_name_list);
+    region_name_list = get_region_name_list_from_adjacent();
   elseif (faction:name() == "3k_dlc05_faction_sun_ce") then
     -- 194孙策, 选择初始区域的临近区域
-    region_list_normal = region_list_adjacent_normal;
-    for r = 1, #region_list_normal do region_name_list[r] = region_list_normal[r]:name(); end
-    out(
-      "tommy_randomize_all_characters_for_faction() | from adjacent region_list:num_items():" .. region_list:num_items() .. " -> " ..
-        #region_name_list);
-  elseif (is_faction_has_no_region) then 
+    region_name_list = get_region_name_list_from_adjacent();
+  elseif (is_faction_has_no_region) then
     -- 如果一个阵营没有初始地区，把地区池指定为全地图范围（然后就会全地图随机找个位置
-    region_list_normal = region_list_world_normal;
-    for r = 1, #region_list_normal do region_name_list[r] = region_list_normal[r]:name(); end
-    out(
-      "tommy_randomize_all_characters_for_faction() | from all region_list:num_items():" .. region_list:num_items() .. " -> " ..
-        #region_name_list);
+    region_name_list = get_region_name_list_from_all();
   elseif (is_faction_has_1_region) then
-    region_list_normal = region_list_adjacent_normal;
-    for r = 1, #region_list_normal do region_name_list[r] = region_list_normal[r]:name(); end
-    out(
-      "tommy_randomize_all_characters_for_faction() | from adjacent region_list:num_items():" .. region_list:num_items() .. " -> " ..
-        #region_name_list);
+    region_name_list = get_region_name_list_from_adjacent();
   else
     for r = 1, #region_list_normal do region_name_list[r] = region_list_normal[r]:name(); end
-    out(
-      "tommy_randomize_all_characters_for_faction() | from own region_list:num_items():" .. region_list:num_items() .. " -> " ..
-        #region_name_list);
+    out("tommy_randomize_all_characters_for_faction() | " ..faction:name().. " | from own," .. region_list:num_items());
   end
 
   if (faction:has_faction_leader()) then faction_leader = faction:faction_leader(); end
@@ -175,15 +182,15 @@ local function tommy_randomize_all_characters_for_faction(context, faction, init
         region_name_list_patched[region_name_list_patched_length + 1] = general:region():name();
         -- 1) 从当前角色所在地区(初始所在地区不等于拥有地区, 例如郑酱)和玩家拥有地区中随机选一个地区
         local position, region_name_random_picked = _get_random_region_position_prevent_duplicated(
-          region_name_list_patched, initial_positions_of_region_expand
-        );
+                                                      region_name_list_patched, initial_positions_of_region_expand);
         out("tommy_randomize_all_characters_for_faction() | before teleport, pick:" .. region_name_random_picked ..
               " found_position:" .. _get_bool_str(not is_nil(position)));
         if (not is_nil(position)) then
-          math.randomseed(os.time());
           local final_x, final_y = unpack(position);
+          final_x = final_x + random_split_2(0.66, 0.66);
+          final_y = final_y + random_split_2(0.66, 0.66);
           out("tommy_randomize_all_characters_for_faction() | before teleport, pick:" .. region_name_random_picked ..
-              " found_position:" .. final_x .. "," .. final_y);
+                " found_position:" .. final_x .. "," .. final_y);
           cm:teleport_character(general, final_x, final_y);
         end
       end
@@ -310,6 +317,20 @@ local function _get_all_valid_positions_of_region(faction_list)
   return position_region_mapping;
 end
 
+local REGION_DONT_SPAWN_TOO_MUCH = {
+  ["3k_main_bohai_capital"] = true,
+  ["3k_main_dongjun_resource_1"] = true,
+  ["3k_main_penchang_resource_1"] = true,
+  ["3k_main_penchang_resource_2"] = true,
+  -- 宝岛台湾
+  ["3k_main_yizhou_resource_2"] = true,
+  ["3k_main_yizhou_resource_1"] = true,
+  ["3k_main_yizhou_island_capital"] = true,
+  -- 宝岛海南
+  ["3k_main_hepu_capital"] = true,
+  ["3k_main_hepu_resource_1"] = true
+}
+
 -- main function
 local function tommy_randomize_start(context)
   out("tommy_randomize_start() | START of script ");
@@ -335,37 +356,51 @@ local function tommy_randomize_start(context)
       local random_offset_y = 0;
       if (is_default) then
         count = count + 1;
-        math.randomseed(os.time())
-        random_offset_x = math.random() * 3 - 6;
-        math.randomseed(os.time())
-        random_offset_y = math.random() * 3 - 6;
+        random_offset_x = random_split_2(0.6, 3);
+        random_offset_y = random_split_2(0.6, 3);
         position_list_expand[#position_list_expand + 1] = {x + random_offset_x, y + random_offset_y}
         out("tommy_randomize_start() | " .. region_name .. ": " .. x + random_offset_x .. "," .. y + random_offset_y ..
-            ", expanded");
+              ", expanded");
       else
         position_list_expand[#position_list_expand + 1] = {x, y}
-        count = count + 3;
-        math.randomseed(os.time())
-        random_offset_x = math.random() * 5 - 10;
-        math.randomseed(os.time())
-        random_offset_y = math.random() * 5 - 10;
-        position_list_expand[#position_list_expand + 1] = {x + random_offset_x, y + random_offset_y}
-        out("tommy_randomize_start() | " .. region_name .. ": " .. x + random_offset_x .. "," .. y + random_offset_y ..
-            ", expanded");
-        math.randomseed(os.time())
-        random_offset_x = math.random() * 10 - 20;
-        math.randomseed(os.time())
-        random_offset_y = math.random() * 10 - 20;
-        position_list_expand[#position_list_expand + 1] = {x + random_offset_x, y + random_offset_y}
-        out("tommy_randomize_start() | " .. region_name .. ": " .. x + random_offset_x .. "," .. y + random_offset_y ..
-            ", expanded");
-        math.randomseed(os.time())
-        random_offset_x = math.random() * 20 - 40;
-        math.randomseed(os.time())
-        random_offset_y = math.random() * 20 - 40;
-        position_list_expand[#position_list_expand + 1] = {x + random_offset_x, y + random_offset_y}
-        out("tommy_randomize_start() | " .. region_name .. ": " .. x + random_offset_x .. "," .. y + random_offset_y ..
-            ", expanded");
+        if (not is_nil(REGION_DONT_SPAWN_TOO_MUCH[region_name])) then
+          count = count + 1;
+          random_offset_x = random_split_2(0.6, 1);
+          random_offset_y = random_split_2(0.6, 1);
+          position_list_expand[#position_list_expand + 1] = {x + random_offset_x, y + random_offset_y}
+          out(
+            "tommy_randomize_start() | " .. region_name .. ": " .. x + random_offset_x .. "," .. y + random_offset_y ..
+              ", expanded");
+          count = count + 1;
+          random_offset_x = random_split_2(1, 1.6);
+          random_offset_x = random_split_2(1, 1.6);
+          position_list_expand[#position_list_expand + 1] = {x + random_offset_x, y + random_offset_y}
+          out(
+            "tommy_randomize_start() | " .. region_name .. ": " .. x + random_offset_x .. "," .. y + random_offset_y ..
+              ", expanded");
+        else
+          count = count + 1;
+          random_offset_x = random_split_2(0.6, 5);
+          random_offset_y = random_split_2(0.6, 5);
+          position_list_expand[#position_list_expand + 1] = {x + random_offset_x, y + random_offset_y}
+          out(
+            "tommy_randomize_start() | " .. region_name .. ": " .. x + random_offset_x .. "," .. y + random_offset_y ..
+              ", expanded");
+          count = count + 1;
+          random_offset_x = random_split_2(2, 6.6);
+          random_offset_y = random_split_2(2, 6.6);
+          position_list_expand[#position_list_expand + 1] = {x + random_offset_x, y + random_offset_y}
+          out(
+            "tommy_randomize_start() | " .. region_name .. ": " .. x + random_offset_x .. "," .. y + random_offset_y ..
+              ", expanded");
+          count = count + 1;
+          random_offset_x = random_split_2(2, 10);
+          random_offset_y = random_split_2(2, 10);
+          position_list_expand[#position_list_expand + 1] = {x + random_offset_x, y + random_offset_y}
+          out(
+            "tommy_randomize_start() | " .. region_name .. ": " .. x + random_offset_x .. "," .. y + random_offset_y ..
+              ", expanded");
+        end
       end
     end
     out("tommy_randomize_start() | " .. region_name .. " has " .. count);
